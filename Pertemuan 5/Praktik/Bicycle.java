3d52a353664ff02996b3d0a590c859b06551fd3b
BrylianFandhi <76621303+BrylianFandhi@users.noreply.github.com>
2021-04-05 11:20:42 +0700

    P5

---
+++ Pertemuan 5/Praktik/Bicycle.java
@@ -0,0 +1,10 @@
+package Praktik;
+
+public class Bicycle extends Vehicle{
+    
+    Bicycle(){}
+
+    public void ringBell() {
+        System.out.println("Kring...kring...\nkring....kring.....");
+    }
+}
